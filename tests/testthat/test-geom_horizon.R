161cd13193a492233670e2c5deb8763c7ebd7049
rivasiker <ikerrivas96@gmail.com>
2021-09-03 13:06:54 +0200

    Update first test

--- tests/testthat/test-geom_horizon.R
+++ tests/testthat/test-geom_horizon.R
@@ -2,8 +2,7 @@
 test_that("NAs are not dropped from the data", {
   df <- tibble(x = 1:5, y = c(1, 2, NA, 4, 5))
 
-  p <- ggplot(df, aes(x))+
-    geom_ribbon(aes(ymin = y - 1, ymax = y + 1))
+  p <- ggplot(df, aes(x, y))+geom_horizon()
 
   expect_equal(sum(is.na(layer_data(p)$ymax)), length(unique(layer_data(p)$group)))
 })
