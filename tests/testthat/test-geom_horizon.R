7f79198319e293ad46d3d480378dac7a28f9e5dd
rivasiker <ikerrivas96@gmail.com>
2021-09-07 14:48:59 +0200

    Code remaining tests.

--- tests/testthat/test-geom_horizon.R
+++ tests/testthat/test-geom_horizon.R
@@ -123,6 +123,27 @@ test_that("The integer horizonscale works", {
                10)
 })
 
+test_that("The custom horizonscale works", {
+  df <- data.frame(x = 1:10, y = c(1, 3, 5, 8, 1, 2, 6, 9, 2, 2))
+
+  p <- ggplot(df, aes(x, y))+
+    geom_horizon(horizonscale = c(1, 3, 6, 8))
+
+  expect_equal(levels(layer_data(p)$Cutpoints),
+               c("[6, 8)", "[5, 6)", "[3, 5)", "[1, 3)"))
+})
+
+test_that("The custom horizonscale works with numeric origin", {
+  df <- data.frame(x = 1:10, y = c(1, 3, 5, 8, 1, 2, 6, 9, 2, 2))
+
+  p <- ggplot(df, aes(x, y))+
+    geom_horizon(horizonscale = c(1, 3, 6, 8),
+                 origin = 2)
+
+  expect_equal(levels(layer_data(p)$Cutpoints),
+               c("[6, 8)", "[3, 6)", "[2, 3)", "[1, 2)"))
+})
+
 test_that("The processed table has the right dimensions", {
   df <- data.frame(x = 1:10,
                xend = (1:10)+0.999,
@@ -144,6 +165,32 @@ test_that("The processed table has the right dimensions with xend", {
 
   expect_equal(nrow(layer_data(p)),
                nrow(df)*2*length(levels(layer_data(p)$Cutpoints)))
+})
+
+
+test_that("Mirror works", {
+  df <- data.frame(x = 1:10,
+                   y = c(1, 3, 5, 8, 1, 2, 6, 9, 2, 2))
+
+  p <- ggplot(df, aes(x = x, y = y))+geom_horizon(mirror = T)
+
+  expect_true(all(layer_data(p)$ymax == 1))
+})
+
+
+test_that("Reverse works", {
+  df <- data.frame(x = 1:10,
+                   y = c(1, 3, 5, 8, 1, 2, 6, 9, 2, 2))
+
+  p <- ggplot(df, aes(x = x, y = y))+geom_horizon(reverse = T)
+
+  expect_true(
+    all(
+      layer_data(p)$ymin[str_detect(layer_data(p)$Categories, 'neg')] == 1))
+
+  expect_true(
+    all(
+      layer_data(p)$ymin[str_detect(layer_data(p)$Categories, 'pos')] == 0))
 })
 
 
